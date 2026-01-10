ed49dcb06455acfda36399a0f7f474f053f25122
Dhairya <dhairyaparikh1002@gmail.com>
2026-01-10 12:36:41 +0530

    Initialized express backend. Mian Entry file:Index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const EmployeeRoutes = require('./api/employees');
+const DepartmentRoutes = require('./api/departments');
+const AuthRoutes = require('./api/auth');
+const connect_to_mongo = require('./db_connection');
+const port = 3000
+require('dotenv').config();
+
+
+
+
+connect_to_mongo();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+app.use(express.json());
+// API Routes for an http server
+app.use('/api/auth', AuthRoutes);
+app.use('/api/employees', EmployeeRoutes);
+app.use('/api/departments', DepartmentRoutes);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
