55235d062411374879f7f612e00950b931bf280e
Dhairya <dhairyaparikh1002@gmail.com>
2026-01-11 00:17:39 +0530

    Added database connectivity for the application.

---
+++ db_connection.js
@@ -0,0 +1,17 @@
+const mongoose  = require('mongoose');
+require('dotenv').config();
+
+
+// Connection with mongoDB
+const connect_to_mongo = async () => {
+    try{
+        // await mongoose.connect('mongodb+srv://dhairyaparikh1002_db_user:SHmWHCBXt0kSGhHQ@clusterbackeddb.t1wzium.mongodb.net/Apollonia_Dental_HCM?retryWrites=true&w=majority&appName=ClusterBackedDB');
+        await mongoose.connect(`${process.env.MONGODB_URL}`);     //CODE TO BE ACTIVATED BEFORE MOVING TO PRODUCTION
+        console.log("Connected to Mongo DB successflly!");
+    } 
+    catch(err){
+        console.log(err);
+    }
+}
+
+module.exports = connect_to_mongo;
