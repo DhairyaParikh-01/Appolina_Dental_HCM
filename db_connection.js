beba56446a1cb03c280f41e3dfe2f19946712a9a
Dhairya <dhairyaparikh1002@gmail.com>
2026-02-01 13:31:36 +0530

    Fix: Updated dbconnection.js for serverless architrecture

--- db_connection.js
+++ db_connection.js
@@ -1,17 +1,47 @@
-const mongoose  = require('mongoose');
-require('dotenv').config();
+// const mongoose  = require('mongoose');
+// require('dotenv').config();
 
 
-// Connection with mongoDB
-const connect_to_mongo = async () => {
-    try{
-        // await mongoose.connect('mongodb+srv://dhairyaparikh1002_db_user:SHmWHCBXt0kSGhHQ@clusterbackeddb.t1wzium.mongodb.net/Apollonia_Dental_HCM?retryWrites=true&w=majority&appName=ClusterBackedDB');
-        await mongoose.connect(`${process.env.MONGODB_URL}` , {family: 4});     //CODE TO BE ACTIVATED BEFORE MOVING TO PRODUCTION
-        console.log("Connected to Mongo DB successflly!");
-    } 
-    catch(err){
-        console.log(err);
-    }
+// // Connection with mongoDB
+// const connect_to_mongo = async () => {
+//     try{
+//         // await mongoose.connect('mongodb+srv://dhairyaparikh1002_db_user:SHmWHCBXt0kSGhHQ@clusterbackeddb.t1wzium.mongodb.net/Apollonia_Dental_HCM?retryWrites=true&w=majority&appName=ClusterBackedDB');
+//         await mongoose.connect(`${process.env.MONGODB_URL}` , {family: 4});     //CODE TO BE ACTIVATED BEFORE MOVING TO PRODUCTION
+//         console.log("Connected to Mongo DB successflly!");
+//     } 
+//     catch(err){
+//         console.log(err);
+//     }
+// }
+
+// module.exports = connect_to_mongo;
+
+
+// lib/mongo.js
+
+/*
+Refactored code to connect with MongoDB spcefically for serverless architrecture like Vercel-----------------------------------------
+*/
+
+import mongoose from "mongoose";
+
+let cached = global._mongoose;
+
+if (!cached) {
+  cached = global._mongoose = { conn: null, promise: null };
 }
 
-module.exports = connect_to_mongo;
+export async function connectDB() {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(process.env.MONGODB_URL, {
+      bufferCommands: false,
+      maxPoolSize: 5,
+      family: 4
+    });
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
